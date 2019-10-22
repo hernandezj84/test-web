7583a56b38fe59638eb4e4e11b9b11b3aaeb4489
Jonathan Hernandez <hernandezj84@gmail.com>
2019-10-22 15:51:29 -0500

    Add interaction

--- test/specs/basic.js
+++ test/specs/basic.js
@@ -6,4 +6,19 @@ describe("SimpleCalculator web page", () => {
     const title = browser.getTitle();
     expect(title).toBe("SimpleCalculator");
   });
+  test("sum two values 1+1 should be 2", () => {
+    $("#operation")
+      .setValue("1+1")
+      .then(() => {
+        $("#calcButton")
+          .click()
+          .then(() => {
+            $("#result")
+              .getValue()
+              .then(value => {
+                expect(value).toBe("2");
+              });
+          });
+      });
+  });
 });
